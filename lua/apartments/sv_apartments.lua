3e309f55326cf622518c3d4e59d929f407fecccc
Python1320 <python1320+gh@gmail.com>
2024-07-11 20:39:04 +0300

    Nocollide by default (TODO: add per room option to allow colliding)

--- lua/apartments/sv_apartments.lua
+++ lua/apartments/sv_apartments.lua
@@ -452,3 +452,53 @@ hook.Add("PlayerUse", tag, function(ply, ent)
 
 	return false
 end)
+
+local function feature_nocollide()
+	local Tag = 'apartment_collide'
+	
+	local dbg = function(...)
+		if not DEBUG then return end
+		Msg('[' .. Tag .. '] ')
+		print(...)
+	end
+	
+	local function ApartmentEnter(ent, trigger, room)
+		dbg('IN', ent, 'owner=', room and room.tenant)
+		if not ent:IsPlayer() then return end
+		if hook.Run('CanPlayerNoCollideWithTeammates', ent, true, ent:GetNoCollideWithTeammates()) == false then return end
+		if room.nocollide == false then return end
+		ent:SetNoCollideWithTeammates(true)
+	end
+	
+	hook.Add('ApartmentEnter', Tag, ApartmentEnter)
+	
+	local function ApartmentLeave(ent, trigger, room)
+		dbg('OUT', ent, 'owner=', room and room.tenant)
+		if not ent:IsPlayer() then return end
+		if not ent:GetNoCollideWithTeammates() then return end
+		if hook.Run('CanPlayerNoCollideWithTeammates', ent, false, true) == false then return end
+		ent:SetNoCollideWithTeammates(false)
+	
+		timer.Simple(0.4, function()
+			if not ent:IsValid() then return end
+			ent:UnStuck()
+		end)
+	end
+	
+	hook.Add('ApartmentLeave', Tag, ApartmentLeave)
+	
+	function ms.Apartments.SetNoCollide(num, set)
+		set = set == nil and true or set
+		local room = isnumber(num) and ms.Apartments.List[num] or num
+		room.nocollide = set
+	
+		for ply, _ in pairs(room.trigger:GetPlayers()) do
+			if set then
+				ApartmentEnter(ply, room.trigger, room)
+			else
+				ApartmentLeave(ply, room.trigger, room)
+			end
+		end
+	end
+end
+feature_nocollide()
