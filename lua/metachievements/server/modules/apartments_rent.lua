09991bd19162cc4c5e1d2d0876f3c15d38fe05e3
Juho <31177497+jule14@users.noreply.github.com>
2024-12-31 18:33:48 +0200

    add achievement

---
+++ lua/metachievements/server/modules/apartments_rent.lua
@@ -0,0 +1,19 @@
+if not MetAchievements then return end
+
+local tag = "MetAchievements"
+local id = "apartments_rent"
+
+resource.AddFile("materials/metachievements/" .. id .. "/s1/icon.png")
+
+MetAchievements.RegisterAchievement(id, {
+    title = "Antisocial",
+    description = "You joined a multiplayer server only to hole up in a private room.",
+})
+
+hook.Add("ApartmentEnter", ("%s_%s"):format(tag, id), function(ent, trigger, room)
+    if not ent:IsPlayer() or MetAchievements.HasAchievement(ent, id) then return end
+
+    if ent == room.tenant then
+        MetAchievements.UnlockAchievement(ent, id)
+    end
+end)
